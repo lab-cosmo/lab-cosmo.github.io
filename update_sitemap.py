bd48b497e0e4314181d1b1f5a1f039c626a8c374
Michele Ceriotti <michele.ceriotti@gmail.com>
2024-09-03 00:34:11 +0200

    Also include update frequency

--- update_sitemap.py
+++ update_sitemap.py
@@ -5,6 +5,8 @@ something on github pages.
 
 import requests
 import xml.etree.ElementTree as ET
+from datetime import datetime, timedelta
+
 
 org_name = "lab-cosmo"
 
@@ -19,6 +21,18 @@ def get_last_commit_date(repo_name):
             last_commit_date = commits[0]['commit']['committer']['date']
             return last_commit_date.split('T')[0]  # Return just the date part
     return None
+
+def determine_changefreq(last_commit_date):
+    if last_commit_date:
+        date = datetime.strptime(last_commit_date, "%Y-%m-%d")
+        now = datetime.utcnow()
+        if date < now - timedelta(days=365):
+            return "yearly"
+        elif date < now - timedelta(days=30):
+            return "monthly"
+        else:
+            return "weekly"
+    return "monthly"  # Default to monthly if no commit date is available    
 
 # GitHub API URL to list public repositories in the organization
 api_url = f"https://api.github.com/orgs/lab-cosmo/repos?per_page=100&type=public"
@@ -43,9 +57,11 @@ if response.status_code == 200:
         response = requests.get(url, timeout=10)
         if response.status_code == 200:
            last_commit = get_last_commit_date(repo_name)
-           print(f"URL OK: {url}; Last commit {last_commit}")
+           freq = determine_changefreq(last_commit)
+           print(f"URL OK: {url}; Last commit {last_commit}; Update {freq}")
            
-           urls.append({"url": url, "date": last_commit})
+           urls.append({"url": url, "date": last_commit,
+                        "freq": freq})
         elif response.status_code == 404:
            print(f"URL Not Found (404): {url}")
 
@@ -53,13 +69,17 @@ if response.status_code == 200:
     urlset = ET.Element("urlset", xmlns="http://www.sitemaps.org/schemas/sitemap/0.9")
     
     for url_info in urls:
-        url, date = url_info["url"], url_info["date"]
+        url, date, freq = url_info["url"], url_info["date"], url_info["freq"]
         url_element = ET.SubElement(urlset, "url")
         loc = ET.SubElement(url_element, "loc")
         loc.text = url
         
         lastmod = ET.SubElement(url_element, "lastmod")
         lastmod.text = date
+                
+        changefreq = ET.SubElement(url_element, "changefreq")
+        changefreq.text = freq
+
     
     # Save the XML sitemap to a file
     tree = ET.ElementTree(urlset)
